afca2257cb896e25cb70fa9b7e93557ec38b5ec5
plotnikovaal <56859386+plotnikovaal@users.noreply.github.com>
2019-10-22 12:11:37 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
     res.render('index', {title: 'Заголовок', message: 'Тут текст!'});
 });
 
-app.get('/page3', function (req, res) {
-    res.render('page2', {title: 'Страница 2', message: 'Текст второй страницы'});
+app.get('/page2', function (req, res) {
+    res.render('page3', {title: 'Страница 2', message: 'Текст второй страницы'});
 });
 
 app.listen(port, function() {
